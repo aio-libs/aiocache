3750a01648caa5f6e7da768260adc836e4dc9137
argaen <manu.mirandad@gmail.com>
2016-11-04 18:12:35 +0100

    Added default cache, set_defaults cache param now is optional

--- aiocache/__init__.py
+++ aiocache/__init__.py
@@ -21,8 +21,8 @@ DEFAULT_NAMESPACE = None
 DEFAULT_KWARGS = {}
 
 
-def set_defaults(cache, namespace=None, serializer=None, policy=None, **kwargs):
-    aiocache.DEFAULT_CACHE = cache
+def set_defaults(cache=None, namespace=None, serializer=None, policy=None, **kwargs):
+    aiocache.DEFAULT_CACHE = cache or SimpleMemoryCache
     aiocache.DEFAULT_NAMESPACE = namespace or ""
     aiocache.DEFAULT_KWARGS = kwargs
     aiocache.DEFAULT_SERIALIZER = serializer or DefaultSerializer
