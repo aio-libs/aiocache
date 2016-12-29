365b55aaa141f3809ee8bc7c7ca8386e4a47ec9f
argaen <manu.mirandad@gmail.com>
2016-12-29 13:34:27 +0100

    0.2.2 version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aiocache",
-    version="0.2.1",
+    version="0.2.2",
     author="Manuel Miranda",
     url="https://github.com/argaen/aiocache",
     author_email="manu.mirandad@gmail.com",
