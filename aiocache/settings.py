46a00eae8d27935915aa69846523b20047588eb4
argaen <manu.mirandad@gmail.com>
2017-05-07 13:22:40 +0200

    Revert "Revert "Revert "Deprecate settings module"""
    This reverts commit 30c981879458500d7103188e0a448cd66831506c.

--- aiocache/settings.py
+++ aiocache/settings.py
@@ -1,5 +1,4 @@
-from aiocache import caches
-from aiocache.log import logger
+from copy import deepcopy
 
 
 class settings:
@@ -21,15 +20,60 @@ class settings:
 
     @classmethod
     def get_alias(cls, alias):
-        logger.warning("Deprecated, please use 'aiocache.caches.get_config[alias]'")
-        return caches.get_alias_config(alias)
+        return cls._config[alias]
 
     @classmethod
     def get_config(cls):
-        logger.warning("Deprecated, please use 'aiocache.caches.get_config'")
-        return caches.get_config()
+        """
+        Return copy of current stored config
+        """
+        return deepcopy(cls._config)
 
     @classmethod
     def set_config(cls, config):
-        logger.warning("Deprecated, please use 'aiocache.caches.get_config'")
-        return caches.set_config(config)
+        """
+        Set (override) the default settings for aiocache from a dict-like structure.
+        The structure is the following::
+
+            {
+                'default': {
+                    'cache': "aiocache.SimpleMemoryCache",
+                    'serializer': {
+                        'class': "aiocache.serializers.DefaultSerializer"
+                    }
+                },
+                'redis_alt': {
+                    'cache': "aiocache.RedisCache",
+                    'endpoint': "127.0.0.10",
+                    'port': 6378,
+                    'serializer': {
+                        'class': "aiocache.serializers.PickleSerializer"
+                    },
+                    'plugins': [
+                        {'class': "aiocache.plugins.HitMissRatioPlugin"},
+                        {'class': "aiocache.plugins.TimingPlugin"}
+                    ]
+                }
+            }
+
+        'default' key must always exist when passing a new config. Default configuration
+        is::
+
+            {
+                'default': {
+                    'cache': "aiocache.SimpleMemoryCache",
+                    'serializer': {
+                        'class': "aiocache.serializers.DefaultSerializer"
+                    }
+                }
+            }
+
+        You can set your own classes there.
+        The class params accept both str and class types.
+
+        All keys in the config are optional, if they are not passed the defaults
+        for the specified class will be used.
+        """
+        if "default" not in config:
+            raise ValueError("default config must be provided")
+        cls._config = config
