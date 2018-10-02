a221f53b2e02880be06a193d319754fe958143bf
argaen <manu.mirandad@gmail.com>
2018-10-02 17:30:22 +0200

    Add python 3.7 to CI

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Framework :: AsyncIO',
     ],
     packages=find_packages(),
