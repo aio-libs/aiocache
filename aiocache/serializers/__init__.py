e31fca5bfc7d930cbebc56f6714fb67920944c6a
argaen <manu.mirandad@gmail.com>
2019-01-09 20:23:09 +0100

    Move serializers file to its own module

---
+++ aiocache/serializers/__init__.py
@@ -0,0 +1,17 @@
+from .serializers import (
+    BaseSerializer,
+    NullSerializer,
+    StringSerializer,
+    PickleSerializer,
+    JsonSerializer,
+    MsgPackSerializer,
+)
+
+__all__ = (
+    "BaseSerializer",
+    "NullSerializer",
+    "StringSerializer",
+    "PickleSerializer",
+    "JsonSerializer",
+    "MsgPackSerializer",
+)
