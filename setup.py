628a575b95f8b8e94a85309f68dd0f679a708b85
Manuel Miranda <manu.mirandad@gmail.com>
2020-10-15 13:38:49 +0200

    Add support for Python 3.9 (#514)

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Framework :: AsyncIO',
     ],
     packages=find_packages(),
