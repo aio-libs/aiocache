766566f009d294c0067bf6862ac3ef6bb23777ee
unaiwillr <will.randall@unai.com>
2021-09-22 11:08:04 +0100

    Add redis SSL support

--- aiocache/backends/redis.py
+++ aiocache/backends/redis.py
@@ -58,6 +58,7 @@ class RedisBackend:
         port=6379,
         db=0,
         password=None,
+        ssl=None,
         pool_min_size=1,
         pool_max_size=10,
         loop=None,
@@ -69,6 +70,7 @@ class RedisBackend:
         self.port = int(port)
         self.db = int(db)
         self.password = password
+        self.ssl = ssl
         self.pool_min_size = int(pool_min_size)
         self.pool_max_size = int(pool_max_size)
         self.create_connection_timeout = (
@@ -209,6 +211,7 @@ class RedisBackend:
                 kwargs = {
                     "db": self.db,
                     "password": self.password,
+                    "ssl": self.ssl,
                     "loop": self._loop,
                     "encoding": "utf-8",
                     "minsize": self.pool_min_size,
