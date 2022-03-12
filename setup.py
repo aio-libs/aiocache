49709548ee9099ebd0feae16cc07f5e56a8c903d
Sam Bull <aa6bs0@sambull.org>
2022-03-12 13:37:13 +0000

    Test

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setup(
     long_description=readme,
     classifiers=[
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
