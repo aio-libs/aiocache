b8ceb509cbe9ad307835ddf042488eb2c8e66755
Sam Bull <aa6bs0@sambull.org>
2022-12-27 19:34:49 +0000

    Update test_concurrency.py

--- tests/performance/test_concurrency.py
+++ tests/performance/test_concurrency.py
@@ -33,12 +33,11 @@ def test_concurrency_error_rates(server):
         raise
 
     m = re.search(r"Failed requests:\s+([0-9]+)", result.stdout)
-    assert False, "Missing output from ab: " + result.stdout
+    assert m, "Missing output from ab: " + result.stdout
     failed_requests = int(m.group(1))
 
     m = re.search(r"Non-2xx responses:\s+([0-9]+)", result.stdout)
-    assert m, "Missing output from ab:" + result.stdout
-    non_200 = int(m.group(1))
+    non_200 = int(m.group(1)) if m else 0
 
     print(failed_requests / total_requests, non_200 / total_requests)
     assert failed_requests / total_requests < 0.75
