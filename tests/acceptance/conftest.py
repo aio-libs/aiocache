821ea77f61f1e957d7df5dfdd3f7021ae54c8e18
Sam Bull <aa6bs0@sambull.org>
2022-12-28 16:01:21 +0000

    Update conftest.py

--- tests/acceptance/conftest.py
+++ tests/acceptance/conftest.py
@@ -17,36 +17,10 @@ def reset_caches():
     )
 
 
-@pytest.fixture
-def redis_cache(event_loop):
-    cache = Cache(Cache.REDIS, namespace="test")
+@pytest.fixture(params=(Cache.MEMORY, Cache.MEMCACHED, Cache.REDIS))
+async def cache(request):
+    cache = Cache(request.param, namespace="test")
     yield cache
 
-    for key in Keys:
-        event_loop.run_until_complete(cache.delete(key))
-    event_loop.run_until_complete(cache.close())
-
-
-@pytest.fixture
-def memory_cache(event_loop):
-    cache = Cache(namespace="test")
-    yield cache
-
-    for key in Keys:
-        event_loop.run_until_complete(cache.delete(key))
-    event_loop.run_until_complete(cache.close())
-
-
-@pytest.fixture
-def memcached_cache(event_loop):
-    cache = Cache(Cache.MEMCACHED, namespace="test")
-    yield cache
-
-    for key in Keys:
-        event_loop.run_until_complete(cache.delete(key))
-    event_loop.run_until_complete(cache.close())
-
-
-@pytest.fixture(params=["redis_cache", "memory_cache", "memcached_cache"])
-def cache(request):
-    return request.getfixturevalue(request.param)
+    await asyncio.gather(*(cache.delete(k) for k in Keys))
+    await cache.close()
