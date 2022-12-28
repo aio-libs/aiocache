9ce5f3149615a20af27e8ee04426f76bde4cdc59
Sam Bull <aa6bs0@sambull.org>
2022-12-28 16:15:34 +0000

    Update conftest.py

--- tests/acceptance/conftest.py
+++ tests/acceptance/conftest.py
@@ -19,10 +19,33 @@ def reset_caches():
     )
 
 
-@pytest.fixture(params=(Cache.MEMORY, Cache.MEMCACHED, Cache.REDIS))
-async def cache(request):
-    cache = Cache(request.param, namespace="test")
+@pytest.fixture
+def redis_cache(event_loop):
+    cache = Cache(Cache.REDIS, namespace="test")
     yield cache
 
     await asyncio.gather(*(cache.delete(k) for k in Keys))
     await cache.close()
+
+
+@pytest.fixture
+def memory_cache(event_loop):
+    cache = Cache(namespace="test")
+    yield cache
+
+    await asyncio.gather(*(cache.delete(k) for k in Keys))
+    await cache.close()
+
+
+@pytest.fixture
+def memcached_cache(event_loop):
+    cache = Cache(Cache.MEMCACHED, namespace="test")
+    yield cache
+
+    await asyncio.gather(*(cache.delete(k) for k in Keys))
+    await cache.close()
+
+
+@pytest.fixture(params=("redis_cache", "memory_cache", "memcached_cache"))
+def cache(request):
+    return request.getfixturevalue(request.param)
