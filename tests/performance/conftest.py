eb5b05801d4c24872a8ce35a31aa83cc0d261ecf
Sam Bull <aa6bs0@sambull.org>
2022-12-28 19:10:38 +0000

    Update conftest.py

--- tests/performance/conftest.py
+++ tests/performance/conftest.py
@@ -4,7 +4,7 @@ from aiocache import Cache
 
 
 @pytest.fixture
-def redis_cache():
+async def redis_cache():
     # redis connection pool raises ConnectionError but doesn't wait for conn reuse
     #  when exceeding max pool size.
     cache = Cache(Cache.REDIS, namespace="test", pool_max_size=1)
@@ -13,7 +13,7 @@ def redis_cache():
 
 
 @pytest.fixture
-def memcached_cache():
+async def memcached_cache():
     cache = Cache(Cache.MEMCACHED, namespace="test", pool_size=1)
     yield cache
     await cache.close()
