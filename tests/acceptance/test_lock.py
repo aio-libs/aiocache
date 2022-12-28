3798d493a7095f3b8db1624a995248bd2d65414d
Sam Bull <aa6bs0@sambull.org>
2022-12-28 16:22:47 +0000

    Update test_lock.py

--- tests/acceptance/test_lock.py
+++ tests/acceptance/test_lock.py
@@ -46,7 +46,7 @@ class TestRedLock:
         await asyncio.gather(dummy(), dummy(), dummy(), dummy())
         assert cache._add.call_count == 4
         assert cache.get.call_count == 8
-        assert cache.set.call_count == 1
+        assert cache.set.call_count == 1, cache.set.call_args_list
 
     async def test_locking_dogpile_lease_expiration(self, mocker, cache):
         async def dummy() -> None:
