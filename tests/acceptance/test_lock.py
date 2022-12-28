128ad9d959c9fbf8e94d409700954380aae5d511
Sam Bull <aa6bs0@sambull.org>
2022-12-28 02:06:06 +0000

    Update test_lock.py

--- tests/acceptance/test_lock.py
+++ tests/acceptance/test_lock.py
@@ -13,24 +13,19 @@ def lock(cache):
 
 
 class TestRedLock:
-    @pytest.mark.asyncio
     async def test_acquire(self, cache, lock):
         cache.serializer = StringSerializer()
         async with lock:
             assert await cache.get(Keys.KEY_LOCK) == lock._value
 
-    @pytest.mark.asyncio
     async def test_release_does_nothing_when_no_lock(self, lock):
         assert await lock.__aexit__("exc_type", "exc_value", "traceback") is None
 
-    @pytest.mark.asyncio
     async def test_acquire_release(self, cache, lock):
         async with lock:
             pass
         assert await cache.get(Keys.KEY_LOCK) is None
 
-    @pytest.mark.asyncio
-    @pytest.mark.skip(reason="flaky test")
     async def test_locking_dogpile(self, mocker, cache):
         mocker.spy(cache, "get")
         mocker.spy(cache, "set")
@@ -53,28 +48,20 @@ class TestRedLock:
         assert cache.get.call_count == 8
         assert cache.set.call_count == 1
 
-    @pytest.mark.asyncio
     async def test_locking_dogpile_lease_expiration(self, mocker, cache):
-        mocker.spy(cache, "get")
-        mocker.spy(cache, "set")
-
-        async def dummy():
+        async def dummy() -> None:
             res = await cache.get(Keys.KEY)
-            if res is not None:
-                return res
+            assert res is None
 
+            # Lease should expire before cache is set, so res is still None.
             async with RedLock(cache, Keys.KEY, lease=1):
                 res = await cache.get(Keys.KEY)
-                if res is not None:
-                    return res
+                assert res is None
                 await asyncio.sleep(1.1)
                 await cache.set(Keys.KEY, "value")
 
         await asyncio.gather(dummy(), dummy(), dummy(), dummy())
-        assert cache.get.call_count == 8
-        assert cache.set.call_count == 4
 
-    @pytest.mark.asyncio
     async def test_locking_dogpile_propagates_exceptions(self, cache):
         async def dummy():
             async with RedLock(cache, Keys.KEY, lease=1):
@@ -89,19 +76,16 @@ class TestMemoryRedLock:
     def lock(self, memory_cache):
         return RedLock(memory_cache, Keys.KEY, 20)
 
-    @pytest.mark.asyncio
     async def test_release_wrong_token_fails(self, lock):
         await lock.__aenter__()
         lock._value = "random"
         assert await lock.__aexit__("exc_type", "exc_value", "traceback") is None
 
-    @pytest.mark.asyncio
     async def test_release_wrong_client_fails(self, memory_cache, lock):
         wrong_lock = RedLock(memory_cache, Keys.KEY, 20)
         await lock.__aenter__()
         assert await wrong_lock.__aexit__("exc_type", "exc_value", "traceback") is None
 
-    @pytest.mark.asyncio
     async def test_float_lease(self, memory_cache):
         lock = RedLock(memory_cache, Keys.KEY, 0.1)
         await lock.__aenter__()
@@ -114,19 +98,16 @@ class TestRedisRedLock:
     def lock(self, redis_cache):
         return RedLock(redis_cache, Keys.KEY, 20)
 
-    @pytest.mark.asyncio
     async def test_release_wrong_token_fails(self, lock):
         await lock.__aenter__()
         lock._value = "random"
         assert await lock.__aexit__("exc_type", "exc_value", "traceback") is None
 
-    @pytest.mark.asyncio
     async def test_release_wrong_client_fails(self, redis_cache, lock):
         wrong_lock = RedLock(redis_cache, Keys.KEY, 20)
         await lock.__aenter__()
         assert await wrong_lock.__aexit__("exc_type", "exc_value", "traceback") is None
 
-    @pytest.mark.asyncio
     async def test_float_lease(self, redis_cache):
         lock = RedLock(redis_cache, Keys.KEY, 0.1)
         await lock.__aenter__()
@@ -139,19 +120,16 @@ class TestMemcachedRedLock:
     def lock(self, memcached_cache):
         return RedLock(memcached_cache, Keys.KEY, 20)
 
-    @pytest.mark.asyncio
     async def test_release_wrong_token_succeeds_meh(self, lock):
         await lock.__aenter__()
         lock._value = "random"
         assert await lock.__aexit__("exc_type", "exc_value", "traceback") is None
 
-    @pytest.mark.asyncio
     async def test_release_wrong_client_succeeds_meh(self, memcached_cache, lock):
         wrong_lock = RedLock(memcached_cache, Keys.KEY, 20)
         await lock.__aenter__()
         assert await wrong_lock.__aexit__("exc_type", "exc_value", "traceback") is None
 
-    @pytest.mark.asyncio
     async def test_float_lease(self, memcached_cache):
         lock = RedLock(memcached_cache, Keys.KEY, 0.1)
         with pytest.raises(TypeError):
@@ -163,17 +141,14 @@ class TestOptimisticLock:
     def lock(self, cache):
         return OptimisticLock(cache, Keys.KEY)
 
-    @pytest.mark.asyncio
     async def test_acquire(self, cache, lock):
         await cache.set(Keys.KEY, "value")
         async with lock:
             assert lock._token == await cache._gets(cache._build_key(Keys.KEY))
 
-    @pytest.mark.asyncio
     async def test_release_does_nothing(self, lock):
         assert await lock.__aexit__("exc_type", "exc_value", "traceback") is None
 
-    @pytest.mark.asyncio
     async def test_check_and_set_not_existing_never_fails(self, cache, lock):
         async with lock as locked:
             await cache.set(Keys.KEY, "conflicting_value")
@@ -181,7 +156,6 @@ class TestOptimisticLock:
 
         assert await cache.get(Keys.KEY) == "value"
 
-    @pytest.mark.asyncio
     async def test_check_and_set(self, cache, lock):
         await cache.set(Keys.KEY, "previous_value")
         async with lock as locked:
@@ -189,7 +163,6 @@ class TestOptimisticLock:
 
         assert await cache.get(Keys.KEY) == "value"
 
-    @pytest.mark.asyncio
     async def test_check_and_set_fail(self, cache, lock):
         await cache.set(Keys.KEY, "previous_value")
         with pytest.raises(OptimisticLockError):
@@ -197,7 +170,6 @@ class TestOptimisticLock:
                 await cache.set(Keys.KEY, "conflicting_value")
                 await locked.cas("value")
 
-    @pytest.mark.asyncio
     async def test_check_and_set_with_int_ttl(self, cache, lock):
         await cache.set(Keys.KEY, "previous_value")
         async with lock as locked:
@@ -212,7 +184,6 @@ class TestMemoryOptimisticLock:
     def lock(self, memory_cache):
         return OptimisticLock(memory_cache, Keys.KEY)
 
-    @pytest.mark.asyncio
     async def test_check_and_set_with_float_ttl(self, memory_cache, lock):
         await memory_cache.set(Keys.KEY, "previous_value")
         async with lock as locked:
@@ -227,7 +198,6 @@ class TestRedisOptimisticLock:
     def lock(self, redis_cache):
         return OptimisticLock(redis_cache, Keys.KEY)
 
-    @pytest.mark.asyncio
     async def test_check_and_set_with_float_ttl(self, redis_cache, lock):
         await redis_cache.set(Keys.KEY, "previous_value")
         async with lock as locked:
