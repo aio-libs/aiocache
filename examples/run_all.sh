f46f64b8f656f85558b799aed5f22d2183247367
Sam Bull <aa6bs0@sambull.org>
2022-12-31 18:15:23 +0000

    Update run_all.sh

--- examples/run_all.sh
+++ examples/run_all.sh
@@ -4,9 +4,7 @@ pushd "$(dirname "$0")"
 
 for f in *.py; do
   echo "########## Running $f #########"
-  # ResourceWarning fails to exit 1, so we grep for warnings.
-  python -bb -We -X dev -c "import asyncio, os, sys, ${f%.*}; sys.unraisablehook = lambda: os._exit(1); asyncio.run(${f%.*}.main())" || exit 1
-  echo;echo;echo
+  python -bb -We -X dev -c "import asyncio, os, sys, ${f%.*}; sys.unraisablehook = lambda _: os._exit(1); asyncio.run(${f%.*}.main())" || exit 1
 done
 
 popd
