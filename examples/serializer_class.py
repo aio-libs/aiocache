d0e88d598909e68b31f58e7cd0e4bb1cbf1f5bf7
Sam Bull <aa6bs0@sambull.org>
2022-12-31 14:25:48 +0000

    Update serializer_class.py

--- examples/serializer_class.py
+++ examples/serializer_class.py
@@ -15,7 +15,7 @@ class CompressionSerializer(BaseSerializer):
     def dumps(self, value):
         print("I've received:\n{}".format(value))
         compressed = zlib.compress(value.encode())
-        print("But I'm storing:\n{}".format(str(compressed)))
+        print("But I'm storing:\n{!r}".format(compressed))
         return compressed
 
     def loads(self, value):
