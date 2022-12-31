d1cb537035a0179f4b969ee29de623df6930cc89
Sam Bull <aa6bs0@sambull.org>
2022-12-31 18:54:35 +0000

    Update run_all.sh

--- examples/run_all.sh
+++ examples/run_all.sh
@@ -4,7 +4,7 @@ pushd "$(dirname "$0")"
 
 for f in *.py; do
   echo "########## Running $f #########"
-  python -bb -We -X dev -c "import asyncio, os, sys, ${f%.*}; sys.unraisablehook = lambda _: os._exit(1); asyncio.run(${f%.*}.main())" || exit 1
+  python -bb -We -X dev -c "import asyncio, os, sys, ${f%.*}; sys.unraisablehook = lambda _: os._exit(1); asyncio.run(${f%.*}.main()); del ${f%.*}" || exit 1
 done
 
 popd
