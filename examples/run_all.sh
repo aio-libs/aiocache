b250f539f037a9d7dfa4406ccb874112c6c6191e
Sam Bull <aa6bs0@sambull.org>
2022-12-31 15:32:50 +0000

    Update run_all.sh

--- examples/run_all.sh
+++ examples/run_all.sh
@@ -4,7 +4,11 @@ pushd "$(dirname "$0")"
 
 for f in `find . -name '*.py' -not -path "./frameworks/*"`; do
   echo "########## Running $f #########"
-  python -bb -We -X dev $f || exit 1
+  # ResourceWarning fails to exit 1, so we grep for warnings.
+  python -bb -We -X dev $f 2>&1 | tee /dev/tty | ( ! fgrep -i 'warning' > /dev/null ) || exit 1
+  if [ ${PIPESTATUS[0]} -ne 0 ]; then
+    exit 1
+  fi
   echo;echo;echo
 done
 
