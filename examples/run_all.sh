effe17b3ac8db506b7240288ca289cac42d9b33d
Sam Bull <aa6bs0@sambull.org>
2022-12-31 16:06:10 +0000

    Update run_all.sh

--- examples/run_all.sh
+++ examples/run_all.sh
@@ -2,11 +2,10 @@
 
 pushd "$(dirname "$0")"
 
-tty=$(tty)
 for f in `find . -name '*.py' -not -path "./frameworks/*"`; do
   echo "########## Running $f #########"
   # ResourceWarning fails to exit 1, so we grep for warnings.
-  python -bb -We -X dev $f 2>&1 | tee $tty | ( ! fgrep -i 'warning' > /dev/null ) || exit 1
+  python -bb -We -X dev $f 2>&1 | tee /dev/stderr | ( ! fgrep -i 'warning' > /dev/null ) || exit 1
   if [ ${PIPESTATUS[0]} -ne 0 ]; then
     exit 1
   fi
