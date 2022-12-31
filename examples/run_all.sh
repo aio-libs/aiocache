7f618f8075f0ac7eaeb3f07542daaa1379fedc5d
Sam Bull <aa6bs0@sambull.org>
2022-12-31 13:59:50 +0000

    Run examples with warnings

--- examples/run_all.sh
+++ examples/run_all.sh
@@ -4,7 +4,7 @@ pushd "$(dirname "$0")"
 
 for f in `find . -name '*.py' -not -path "./frameworks/*"`; do
   echo "########## Running $f #########"
-  python $f || exit 1
+  python -bb -We -X dev $f || exit 1
   echo;echo;echo
 done
 
