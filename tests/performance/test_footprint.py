5995d64ffd4c8c7680c46949fc9fad2a511a3eca
Padraic Shafer <PShafer@lbl.gov>
2023-02-23 19:44:11 -0800

    redis.asyncio.Redis is generic for type checking purposes, but not at runtime

--- tests/performance/test_footprint.py
+++ tests/performance/test_footprint.py
@@ -8,8 +8,8 @@ import redis.asyncio as redis
 
 
 @pytest.fixture
-async def redis_client() -> AsyncGenerator[redis.Redis[Any], None]:
-    r: redis.Redis[Any]
+async def redis_client() -> "AsyncGenerator[redis.Redis[Any], None]":
+    r: "redis.Redis[Any]"
     async with redis.Redis(host="127.0.0.1", port=6379, max_connections=1) as r:
         yield r
 
