2026f78cb820f606f292546a93ed38fd4672a7fb
Sam Bull <git@sambull.org>
2025-06-08 14:19:03 +0100

    Update valkey.py

--- aiocache/backends/valkey.py
+++ aiocache/backends/valkey.py
@@ -17,7 +17,7 @@ from glide.exceptions import RequestError as IncrbyException
 from aiocache.base import API, BaseCache
 from aiocache.serializers import JsonSerializer
 
-if TYPE_CHECKING:  # pragma: no cover
+if TYPE_CHECKING:
     from aiocache.serializers import BaseSerializer
 
 if sys.version_info >= (3, 11):
