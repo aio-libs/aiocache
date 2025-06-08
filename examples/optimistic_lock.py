43f1222b8703cddcbb6234c0a28a23373a7f6192
amirreza <amir.rsf1380@gmail.com>
2025-06-09 01:59:50 +0330

    alter examples ti use one cache client

--- examples/optimistic_lock.py
+++ examples/optimistic_lock.py
@@ -10,6 +10,7 @@ from aiocache.lock import OptimisticLock, OptimisticLockError
 logger = logging.getLogger(__name__)
 addresses = [NodeAddress("localhost", 6379)]
 config = GlideClientConfiguration(addresses=addresses, database_id=0)
+cache = ValkeyCache(config=config, namespace="main")
 
 
 async def expensive_function():
@@ -18,27 +19,25 @@ async def expensive_function():
     return "result"
 
 
-async def my_view():
-
-    async with ValkeyCache(config=config, namespace="main") as cache:
-        async with OptimisticLock(cache, "key") as lock:
-            result = await expensive_function()
-            try:
-                await lock.cas(result)
-            except OptimisticLockError:
-                logger.warning(
-                    "I failed setting the value because it is different since the lock started!"
-                )
-            return result
+async def my_view(cache):
+    async with OptimisticLock(cache, "key") as lock:
+        result = await expensive_function()
+        try:
+            await lock.cas(result)
+        except OptimisticLockError:
+            logger.warning(
+                "I failed setting the value because it is different since the lock started!"
+            )
+        return result
 
 
 async def concurrent():
-    async with ValkeyCache(config=config, namespace="main") as cache:
+    async with cache:
         await cache.set("key", "initial_value")
-    # All three calls will read 'initial_value' as the value to check and only
-    # the first one finishing will succeed because the others, when trying to set
-    # the value, will see that the value is not the same as when the lock started
-    await asyncio.gather(my_view(), my_view(), my_view())
+        # All three calls will read 'initial_value' as the value to check and only
+        # the first one finishing will succeed because the others, when trying to set
+        # the value, will see that the value is not the same as when the lock started
+        await asyncio.gather(my_view(cache), my_view(cache), my_view(cache))
 
 
 async def test_redis():
--- examples/python_object.py
+++ examples/python_object.py
@@ -11,23 +11,20 @@ addresses = [NodeAddress("localhost", 6379)]
 config = GlideClientConfiguration(addresses=addresses, database_id=0)
 
 
-async def complex_object():
+async def complex_object(cache):
     obj = MyObject(x=1, y=2)
-    async with ValkeyCache(
-        config=config, namespace="main", serializer=PickleSerializer()
-    ) as cache:
-        await cache.set("key", obj)
-        my_object = await cache.get("key")
+    await cache.set("key", obj)
+    my_object = await cache.get("key")
 
     assert my_object.x == 1
     assert my_object.y == 2
 
 
 async def test_python_object():
-    await complex_object()
     async with ValkeyCache(
         config=config, namespace="main", serializer=PickleSerializer()
     ) as cache:
+        await complex_object(cache)
         await cache.delete("key")
         await cache.close()
 
