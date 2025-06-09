d6f1c358c306ebacf3e5b8088178da21e39de96a
Sam Bull <git@sambull.org>
2025-06-09 20:23:36 +0100

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,4 @@ def valkey_config():
     from glide import GlideClientConfiguration, NodeAddress
 
     addresses = [NodeAddress("localhost", 6379)]
-    conf = GlideClientConfiguration(addresses=addresses, database_id=0)
-
-    yield conf
+    return GlideClientConfiguration(addresses=addresses, database_id=0)
