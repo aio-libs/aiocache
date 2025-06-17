871410b5d5f817a8e22a6295526fcba931c5cb0a
rafael <s.akhmedov@proton.me>
2025-06-17 19:07:44 +0500

    merge `ValkeyBackend` into `ValkeyCache`

--- aiocache/backends/valkey.py
+++ aiocache/backends/valkey.py
@@ -1,6 +1,6 @@
 import logging
 import sys
-from typing import Any, Callable, Optional
+from typing import Any, Iterable, Literal, Mapping, Optional, TypedDict, Union, overload
 
 from glide import (
     ConditionalChange,
@@ -12,139 +12,25 @@ from glide import (
 )
 from glide.exceptions import RequestError as IncrbyException
 
-from aiocache.base import BaseCache
-from aiocache.serializers import BaseSerializer, JsonSerializer
+from aiocache.base import BaseCache, BaseCacheArgs, _Conn
+from aiocache.serializers import JsonSerializer
 
 if sys.version_info >= (3, 11):
-    from typing import Self
+    from typing import Self, Unpack
 else:
-    from typing import Any as Self
+    from typing_extensions import Self, Unpack  # noqa: I900
 
 
 logger = logging.getLogger(__name__)
 
 
-class ValkeyBackend(BaseCache[str]):
-    def __init__(self, config: GlideClientConfiguration, **kwargs):
-        self.config = config
-        super().__init__(**kwargs)
-
-    async def __aenter__(self) -> Self:
-        self.client = await GlideClient.create(self.config)
-        return self
-
-    async def __aexit__(self, *args, **kwargs) -> None:
-        await self.client.close()
-
-    async def _get(self, key, encoding="utf-8", _conn=None):
-        value = await self.client.get(key)
-        if encoding is None or value is None:
-            return value
-        return value.decode(encoding)
-
-    _gets = _get
-
-    async def _multi_get(self, keys, encoding="utf-8", _conn=None):
-        values = await self.client.mget(keys)
-        if encoding is None:
-            return values
-        return [v if v is None else v.decode(encoding) for v in values]
-
-    async def _set(self, key, value, ttl=None, _cas_token=None, _conn=None):
-        if isinstance(ttl, float):
-            ttl = ExpirySet(ExpiryType.MILLSEC, int(ttl * 1000))
-        elif ttl:
-            ttl = ExpirySet(ExpiryType.SEC, ttl)
-
-        if _cas_token is not None:
-            return await self._cas(key, value, _cas_token, ttl=ttl, _conn=_conn)
-
-        return await self.client.set(key, value, expiry=ttl) == "OK"
-
-    async def _cas(self, key, value, token, ttl=None, _conn=None):
-        if await self._get(key) == token:
-            return await self.client.set(key, value, expiry=ttl) == "OK"
-        return 0
-
-    async def _multi_set(self, pairs, ttl=None, _conn=None):
-        values = dict(pairs)
-
-        if ttl:
-            await self.__multi_set_ttl(values, ttl)
-        else:
-            await self.client.mset(values)
-
-        return True
-
-    async def __multi_set_ttl(self, values, ttl):
-        transaction = Transaction()
-        transaction.mset(values)
-        ttl, exp = (
-            (int(ttl * 1000), transaction.pexpire)
-            if isinstance(ttl, float)
-            else (ttl, transaction.expire)
-        )
-        for key in values:
-            exp(key, ttl)
-        await self.client.exec(transaction)
-
-    async def _add(self, key, value, ttl=None, _conn=None):
-        kwargs = {"conditional_set": ConditionalChange.ONLY_IF_DOES_NOT_EXIST}
-        if isinstance(ttl, float):
-            kwargs["expiry"] = ExpirySet(ExpiryType.MILLSEC, int(ttl * 1000))
-        elif ttl:
-            kwargs["expiry"] = ExpirySet(ExpiryType.SEC, ttl)
-        was_set = await self.client.set(key, value, **kwargs)
-        if was_set != "OK":
-            raise ValueError(
-                "Key {} already exists, use .set to update the value".format(key)
-            )
-        return was_set
-
-    async def _exists(self, key, _conn=None):
-        return bool(await self.client.exists([key]))
-
-    async def _increment(self, key, delta, _conn=None):
-        try:
-            return await self.client.incrby(key, delta)
-        except IncrbyException:
-            raise TypeError("Value is not an integer") from None
-
-    async def _expire(self, key, ttl, _conn=None):
-        if ttl == 0:
-            return await self.client.persist(key)
-        return await self.client.expire(key, ttl)
-
-    async def _delete(self, key, _conn=None):
-        return await self.client.delete([key])
-
-    async def _clear(self, namespace=None, _conn=None):
-        if not namespace:
-            return await self.client.flushdb()
-
-        _, keys = await self.client.scan(b"0", "{}:*".format(namespace))
-        if keys:
-            return bool(await self.client.delete(keys))
-
-        return True
-
-    async def _raw(self, command, *args, encoding="utf-8", _conn=None, **kwargs):
-        value = await getattr(self.client, command)(*args, **kwargs)
-        if encoding is not None:
-            if command == "get" and value is not None:
-                value = value.decode(encoding)
-        return value
-
-    async def _redlock_release(self, key, value):
-        if await self._get(key) == value:
-            return await self.client.delete([key])
-        return 0
-
-    def build_key(self, key: str, namespace: Optional[str] = None) -> str:
-        return self._str_build_key(key, namespace)
+class _AddKwargs(TypedDict, total=False):
+    conditional_set: ConditionalChange
+    expiry: Optional[ExpirySet]
+    return_old_value: bool
 
 
-class ValkeyCache(ValkeyBackend):
+class ValkeyCache(BaseCache[str]):
     """
     Valkey cache implementation with the following components as defaults:
         - serializer: :class:`aiocache.serializers.JsonSerializer`
@@ -164,23 +50,222 @@ class ValkeyCache(ValkeyBackend):
     NAME = "valkey"
 
     def __init__(
+        self, config: GlideClientConfiguration, **kwargs: Unpack[BaseCacheArgs]
+    ) -> None:
+        self.config = config
+
+        if "serializer" not in kwargs:
+            kwargs["serializer"] = JsonSerializer()
+        if "key_builder" not in kwargs:
+            kwargs["key_builder"] = lambda k, ns: f"{ns}:{k}" if ns else k
+
+        super().__init__(**kwargs)
+
+    async def __aenter__(self) -> Self:
+        self.client = await GlideClient.create(self.config)
+        return self
+
+    async def __aexit__(self, *args: Any, **kwargs: Any) -> None:
+        await self.client.close()
+
+    @overload
+    async def _get(
         self,
-        config: GlideClientConfiguration,
-        serializer: Optional[BaseSerializer] = None,
-        namespace: str = "",
-        key_builder: Callable[[str, str], str] = lambda k, ns: f"{ns}:{k}" if ns else k,
-        **kwargs: Any,
-    ):
-        super().__init__(
-            config,
-            serializer=serializer or JsonSerializer(),
-            namespace=namespace,
-            key_builder=key_builder,
-            **kwargs,
+        key: str,
+        encoding: str = "utf-8",
+        _conn: Union[_Conn, None] = None,
+    ) -> Union[str, None]: ...
+
+    @overload
+    async def _get(
+        self, key: str, encoding: None, _conn: Union[_Conn, None] = None
+    ) -> Union[bytes, None]: ...
+
+    async def _get(
+        self,
+        key: str,
+        encoding: Union[str, None] = "utf-8",
+        _conn: Union[_Conn, None] = None,
+    ) -> Union[str, bytes, None]:
+        value = await self.client.get(key)
+        if encoding is None or value is None:
+            return value
+        return value.decode(encoding)
+
+    _gets = _get
+
+    @overload
+    async def _multi_get(
+        self,
+        keys: list[str],
+        encoding: str = "utf-8",
+        _conn: Union[_Conn, None] = None,
+    ) -> list[Union[str, None]]: ...
+
+    @overload
+    async def _multi_get(
+        self,
+        keys: list[str],
+        encoding: None,
+        _conn: Union[_Conn, None] = None,
+    ) -> list[Union[bytes, None]]: ...
+
+    async def _multi_get(
+        self,
+        keys: list[str],
+        encoding: Union[str, None] = "utf-8",
+        _conn: Union[_Conn, None] = None,
+    ) -> Union[
+        list[Union[str, None]],
+        list[Union[bytes, None]],
+    ]:
+        values = await self.client.mget(keys)  # type: ignore[arg-type]
+        if encoding is None:
+            return values
+        return [v if v is None else v.decode(encoding) for v in values]
+
+    async def _set(
+        self,
+        key: str,
+        value: Union[str, bytes],
+        ttl: Union[float, int, None] = None,
+        _cas_token: Union[str, None] = None,
+        _conn: Union[_Conn, None] = None,
+    ) -> bool:
+        if isinstance(ttl, float):
+            ttl_ = ExpirySet(ExpiryType.MILLSEC, int(ttl * 1000))
+        elif ttl:
+            ttl_ = ExpirySet(ExpiryType.SEC, ttl)
+        else:
+            ttl_ = None
+
+        if _cas_token is not None:
+            return await self._cas(key, value, _cas_token, ttl=ttl_, _conn=_conn)
+
+        return await self.client.set(key, value, expiry=ttl_) == "OK"  # type: ignore[comparison-overlap]
+
+    async def _cas(
+        self,
+        key: str,
+        value: Union[str, bytes],
+        token: str,
+        ttl: Union[ExpirySet, int, None] = None,
+        _conn: Union[_Conn, None] = None,
+    ) -> bool:
+        if await self._get(key) == token:
+            if isinstance(ttl, int):
+                ttl = ExpirySet(ExpiryType.SEC, ttl)
+            return await self.client.set(key, value, expiry=ttl) == "OK"  # type: ignore[comparison-overlap]
+        return False
+
+    async def _multi_set(
+        self,
+        pairs: Iterable[tuple[str, Union[str, bytes]]],
+        ttl: Union[int, None] = None,
+        _conn: Union[_Conn, None] = None,
+    ) -> bool:
+        values = dict(pairs)
+
+        if ttl:
+            await self.__multi_set_ttl(values, ttl)
+        else:
+            await self.client.mset(values)  # type: ignore[arg-type]
+
+        return True
+
+    async def __multi_set_ttl(
+        self, values: Mapping[str, Union[str, bytes]], ttl: int
+    ) -> None:
+        transaction = Transaction()
+        transaction.mset(values)  # type: ignore[arg-type]
+        ttl, exp = (
+            (int(ttl * 1000), transaction.pexpire)
+            if isinstance(ttl, float)
+            else (ttl, transaction.expire)
         )
+        for key in values:
+            exp(key, ttl)
+        await self.client.exec(transaction)
+
+    async def _add(
+        self,
+        key: str,
+        value: Union[str, bytes],
+        ttl: Union[float, int, None] = None,
+        _conn: Union[_Conn, None] = None,
+    ) -> Literal["OK"]:
+        kwargs: _AddKwargs = {
+            "conditional_set": ConditionalChange.ONLY_IF_DOES_NOT_EXIST
+        }
+        if isinstance(ttl, float):
+            kwargs["expiry"] = ExpirySet(ExpiryType.MILLSEC, int(ttl * 1000))
+        elif ttl:
+            kwargs["expiry"] = ExpirySet(ExpiryType.SEC, ttl)
+
+        was_set = await self.client.set(key, value, **kwargs)
+        if was_set != "OK":  # type: ignore[comparison-overlap]
+            raise ValueError(
+                "Key {} already exists, use .set to update the value".format(key)
+            )
+        return was_set  # type: ignore[return-value]
+
+    async def _exists(self, key: str, _conn: Union[_Conn, None] = None) -> bool:
+        return bool(await self.client.exists([key]))
+
+    async def _increment(
+        self, key: str, delta: int, _conn: Union[_Conn, None] = None
+    ) -> int:
+        try:
+            return await self.client.incrby(key, delta)
+        except IncrbyException:
+            raise TypeError("Value is not an integer") from None
+
+    async def _expire(
+        self, key: str, ttl: int, _conn: Union[_Conn, None] = None
+    ) -> bool:
+        if ttl == 0:
+            return await self.client.persist(key)
+        return await self.client.expire(key, ttl)
+
+    async def _delete(self, key: str, _conn: Union[_Conn, None] = None) -> int:
+        return await self.client.delete([key])
+
+    async def _clear(
+        self, namespace: Union[str, None] = None, _conn: Union[_Conn, None] = None
+    ) -> Union[Literal["OK"], bool]:
+        if not namespace:
+            return await self.client.flushdb()
+
+        _, keys = await self.client.scan(b"0", "{}:*".format(namespace))
+        if keys:
+            return bool(await self.client.delete(keys))  # type: ignore[arg-type]
+
+        return True
+
+    async def _raw(
+        self,
+        command: str,
+        *args: Any,
+        encoding: Union[str, None] = "utf-8",
+        _conn: Union[_Conn, None] = None,
+        **kwargs: Any,
+    ) -> Any:
+        value = await getattr(self.client, command)(*args, **kwargs)
+        if encoding is not None:
+            if command == "get" and value is not None:
+                value = value.decode(encoding)
+        return value
+
+    async def _redlock_release(self, key: str, value: Union[str, bytes]) -> int:
+        if await self._get(key) == value:
+            return await self.client.delete([key])
+        return 0
+
+    def build_key(self, key: str, namespace: Optional[str] = None) -> str:
+        return self._str_build_key(key, namespace)
 
     @classmethod
-    def parse_uri_path(cls, path):
+    def parse_uri_path(cls, path: str) -> dict[str, str]:
         """
         Given a uri path, return the Valkey specific configuration
         options in that path string according to iana definition
@@ -195,7 +280,7 @@ class ValkeyCache(ValkeyBackend):
             options["db"] = db
         return options
 
-    def __repr__(self):  # pragma: no cover
+    def __repr__(self) -> str:  # pragma: no cover
         return (
             f"ValkeyCache ({self.client.config.addresses[0].host}"
             f":{self.client.config.addresses[0].port})"
